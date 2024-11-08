a4fb31deaa0c572b9fd37c8de4011d66047ca679
kyllian-delbauche-vinci <kylliandelbauchep@gmail.com>
2024-11-08 11:37:12 +0100

    init

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,6 +1,16 @@
 import { Main } from '../src/app.js';
 
-describe("", function() {    
+const timestamp = 1672531199;
 
-});
+const date = new Date(timestamp * 1000);
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds(); 
 
+let main = new Main();
+
+
+describe("A suite of test for the demo function of the main class", function() { 
+
+    });
+    
