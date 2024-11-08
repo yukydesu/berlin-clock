9dc3d851bc89fcccce0355a9aa58cd3b91297423
kyllian-delbauche-vinci <kylliandelbauchep@gmail.com>
2024-11-08 12:04:57 +0100

    add firstLineOfMinutes

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,16 +1,65 @@
 import { Main } from '../src/app.js';
 
-const timestamp = 1672531199;
+const timestamp = 1672501190;
 
 const date = new Date(timestamp * 1000);
 const hours = date.getHours();
-const minutes = date.getMinutes();
+const minutes = date.getMinutes(); 
 const seconds = date.getSeconds(); 
 
-let main = new Main();
-
-
 describe("A suite of test for the demo function of the main class", function() { 
+
+        // arrange
+        let main = new Main();
+
+        it("should return the first line of minutes", function() {
+            // act
+            let result = main.firstLineOfMinutes(0);
+            // assert
+            expect(result).toBe("-----------");
+        });
+
+        it("should return the first line of minutes", function() {
+            // act
+            let result = main.firstLineOfMinutes(5);
+            // assert
+            expect(result).toBe("Y----------");
+        });
+
+        it("should return the first line of minutes", function() {
+            // act
+            let result = main.firstLineOfMinutes(10);
+            // assert
+            expect(result).toBe("YY---------");
+        });
+
+        it("should return the first line of minutes", function() {
+            // act
+            let result = main.firstLineOfMinutes(15);
+            // assert
+            expect(result).toBe("YYY--------");
+        });
+
+        it("should return the first line of minutes", function() {
+            // act
+            let result = main.firstLineOfMinutes(20);
+            // assert
+            expect(result).toBe("YYYY-------");
+        });
+
+        it("should return the first line of minutes", function() {
+            // act
+            let result = main.firstLineOfMinutes(5 * 11);
+            // assert
+            expect(result).toBe("YYYYYYYYYYY");
+        });
+
+        it("should return the first line of minutes", function() {
+            // act
+            let result = main.firstLineOfMinutes(minutes); 
+            // assert
+            expect(result).toBe("YYYYYYY----");
+        });
 
     });
     
--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
 export class Main {
+
+    // La ligne du dessus représente des bloque de 5 minutes et elle est composée de 11 lampes. Toutes les 3 lampes nous avons une lampe rouge et les autres sont jaunes. 
  
+    firstLineOfMinutes(minutes) {
+        if (this.getFiveMinuteQuarters(minutes) < 1) return "-----------";
+        if (this.getFiveMinuteQuarters(minutes) < 2) return "Y----------";
+        if (this.getFiveMinuteQuarters(minutes) < 3) return "YY---------";
+        if (this.getFiveMinuteQuarters(minutes) < 4) return "YYY--------";
+        if (this.getFiveMinuteQuarters(minutes) < 5) return "YYYY-------";
+        if (this.getFiveMinuteQuarters(minutes) < 6) return "YYYYY------";
+        if (this.getFiveMinuteQuarters(minutes) < 7) return "YYYYYY-----";
+        if (this.getFiveMinuteQuarters(minutes) < 8) return "YYYYYYY----";
+        if (this.getFiveMinuteQuarters(minutes) < 9) return "YYYYYYYY---";
+        if (this.getFiveMinuteQuarters(minutes) < 10) return "YYYYYYYYY--";
+        if (this.getFiveMinuteQuarters(minutes) < 11) return "YYYYYYYYYY-";
+        if (this.getFiveMinuteQuarters(minutes) < 12) return "YYYYYYYYYYY";
+    }
 
 
+    getFiveMinuteQuarters(minutes) {
+        return minutes / 5;
+    }
 }
