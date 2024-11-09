2dad6e30b16c2ff3481af51ec9a23a13d1f3e313
kyllian-delbauche-vinci <106556142+yukydesu@users.noreply.github.com>
2024-11-09 17:04:29 +0100

    Revert "add a last test with 'seconds'"

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -10,8 +10,6 @@ const hours = date.getHours();
 const minutes = date.getMinutes();
 const seconds = date.getSeconds();
 
-console.log("seconds: ", seconds);
-
 // FIRST LINE OF MINUTES
 describe("testing the first line of minutes", function () {
   // arrange
@@ -347,9 +345,5 @@ describe("testing the line of seconds", function () {
 
     it("59 should return 'Y", function(){
         expect(main.lineOfSeconds(59)).toBe("Y");
-    });
-
-    it("seconds should return 'Y", function(){
-        expect(main.lineOfSeconds(seconds)).toBe("Y"); // seconds = 59
     });
 });
