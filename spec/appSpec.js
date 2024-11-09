37fe7968b5007574e7baaab325dd9233fa0d0b3d
kyllian-delbauche-vinci <kylliandelbauchep@gmail.com>
2024-11-09 12:01:01 +0100

    delet l1 functions, firt test of l2 => OK

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -7,115 +7,18 @@ const hours = date.getHours();
 const minutes = date.getMinutes();  
 const seconds = date.getSeconds(); 
 
-describe("testing the first line of minutes", function() { 
-
-        // arrange
-        let main = new Main();
-
-        it("should return the first line of minutes", function() {
-            // act
-            let result = main.firstLineOfMinutes(0);
-            // assert
-            expect(result).toBe("-----------"); // 0 / 5 = 0
-        });
-
-
-        it("should return the first line of minutes", function() {
-            let result = main.firstLineOfMinutes(1);
-
-            expect(result).toBe("-----------"); // 1 / 5 = 0.2
-        });
-
-        it("should return the first line of minutes", function() {
-
-            let result = main.firstLineOfMinutes(4);
-
-            expect(result).toBe("-----------"); // 4 / 5 = 0.8
-        });
-
-
-        it("should return the first line of minutes", function() {
-            let result = main.firstLineOfMinutes(5); // 5 / 5 = 1
-
-            expect(result).toBe("Y----------");
-        });
-
-        it("should return the first line of minutes", function() { // 10 / 5 = 2
-            let result = main.firstLineOfMinutes(10);
-
-            expect(result).toBe("YY---------");
-        });
-
-
-        it("should return the first line of minutes", function() { // 15 / 5 = 3
-            let result = main.firstLineOfMinutes(15);
-
-            expect(result).toBe("YYR--------");
-        });
-
-
-        it("should return the first line of minutes", function() { // 20 / 5 = 4
-            let result = main.firstLineOfMinutes(20);
-
-            expect(result).toBe("YYRY-------");
-        });
-
-        it("should return the first line of minutes", function() {
-            let result = main.firstLineOfMinutes(5 * 11); // 55 / 5 = 11
-
-            expect(result).toBe("YYRYYRYYRYY");
-        });
-
-        it("should return the first line of minutes", function() {
-
-            let result = main.firstLineOfMinutes(minutes); // minutes = 39 => 39 / 5 = 7.8
-
-            expect(result).toBe("YYRYYRY----");
-        });
-
-    });
-
-
 describe("testing the second line of minutes", function() {
             // arrange
             let main = new Main();
     
-            it("should return the second line of minutes", function() {
+            it("with 0 => should return '----''", function() {
                 // act
                 let result = main.secondLineOfMinutes(0);
                 // assert
                 expect(result).toBe("----");
             });
-    
-            it("should return the second line of minutes", function() {
-                let result = main.secondLineOfMinutes();
-    
-                expect(result).toBe("Y---");
-            });
-    
-            it("should return the second line of minutes", function() {
-                let result = main.secondLineOfMinutes();
-    
-                expect(result).toBe("YY--");
-            });
-    
-            it("should return the second line of minutes", function() {
-                let result = main.secondLineOfMinutes();
-    
-                expect(result).toBe("YYY-");
-            });
-    
-            it("should return the second line of minutes", function() {
-                let result = main.secondLineOfMinutes();
-    
-                expect(result).toBe("YYYY");
-            });
-    
-            it("should return the second line of minutes", function() {
-                let result = main.secondLineOfMinutes(minutes);
-    
-                expect(result).toBe("YY--");
-            });
+
+
 
 });
     
